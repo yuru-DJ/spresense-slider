d2c8253beafa1c827177efaacde4bccdba7741c0
Kazuya Izumi <shikibu9419@gmail.com>
2022-03-13 20:59:31 +0900

    Fix arduino file

--- yuru-dj-slider.ino
+++ yuru-dj-slider.ino
@@ -4,14 +4,11 @@
 RPR0521RS rpr0521rs;
 
 void setup() {
-  byte rc;
-
-  Serial.begin(115200);    /*  Modify : set the serial speed to 11500 kbps  */
-  while (!Serial);
+  Serial.begin(115200);
 
   Wire.begin();
 
-  rc = rpr0521rs.init();
+  byte rc = rpr0521rs.init();
 }
 
 void loop() {
@@ -22,23 +19,13 @@ void loop() {
 
   rc = rpr0521rs.get_psalsval(&ps_val, &als_val);
   if (rc == 0) {
-    Serial.print(F("RPR-0521RS (Proximity)     = "));
-    Serial.print(ps_val);
-    Serial.print(F(" [count]"));
     near_far = rpr0521rs.check_near_far(ps_val);
     if (near_far == RPR0521RS_NEAR_VAL) {
-      Serial.println(F(" Near"));
+      Serial.println("NEAR");
     } else {
-      Serial.println(F(" Far"));
-    }
-
-    if (als_val != RPR0521RS_ERROR) {
-      Serial.print(F("RPR-0521RS (Ambient Light) = "));
-      Serial.print(als_val);
-      Serial.println(F(" [lx]"));
-      Serial.println();
+      Serial.println("FAR");
     }
   }
 
-  delay(500);
+  delay(10);
 }
